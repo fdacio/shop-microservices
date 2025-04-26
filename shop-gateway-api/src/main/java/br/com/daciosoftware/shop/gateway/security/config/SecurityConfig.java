7b878001c2d4d7b5de4bb38616bff6426dbd5a4d
Dácio Braga <fdacio@gmail.com>
2025-04-26 01:57:20 -0300

    fix bug token expired and authenticated exceptions

--- shop-gateway-api/src/main/java/br/com/daciosoftware/shop/gateway/security/config/SecurityConfig.java
+++ shop-gateway-api/src/main/java/br/com/daciosoftware/shop/gateway/security/config/SecurityConfig.java
@@ -121,7 +121,6 @@ public class SecurityConfig {
 
     }
 
-
     @Bean
     public ReactiveJwtDecoder jwtDecoder() {
         try {
--- shop-gateway-api/src/main/java/br/com/daciosoftware/shop/gateway/security/exception/CustomAuthenticationEntryPoint.java
+++ shop-gateway-api/src/main/java/br/com/daciosoftware/shop/gateway/security/exception/CustomAuthenticationEntryPoint.java
@@ -8,7 +8,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.jwt.BadJwtException;
+import org.springframework.security.oauth2.jwt.JwtValidationException;
 import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -22,9 +25,12 @@ public class CustomAuthenticationEntryPoint implements ServerAuthenticationEntry
 
     @Override
     public Mono<Void> commence(ServerWebExchange exchange, AuthenticationException exception) {
-        log.info("erro handle {} : {}", exception.getMessage(), exception.getCause().getMessage());
-        //String message = (exception instanceof InvalidBearerTokenException) ? "Token expirado" : "Não autenticado";
+        log.info("error handler: {}",  exception.getCause().toString());
         String message = exception.getMessage();
+        if (exception.getCause() instanceof InsufficientAuthenticationException) message = "Não Autenticado: EntryPoint";
+        if (exception.getCause() instanceof BadJwtException) message = "Token inválido: EntryPoint";
+        if (exception.getCause() instanceof JwtValidationException) message = "Token expirado: EntryPoint";
+
         ServerHttpResponse response = exchange.getResponse();
         response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
         response.setStatusCode(HttpStatus.UNAUTHORIZED);
